95e396749de23be558d466a438706bd1bb575bf0
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-05-01 08:58:14 +0900

    Get token from token.txt

--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+)
+
+// 読み込みバッファのサイズ
+const (
+	BUFSIZE = 1024
 )
 
 func main() {
-	fmt.Println("Hello world")
+	file, err := os.Open(`./token.txt`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	buf := make([]byte, BUFSIZE)
+	for {
+		n, err := file.Read(buf)
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+			break
+		}
+
+		fmt.Print(string(buf[:n]))
+	}
 }
