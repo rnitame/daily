6501e564df43e5286de55c289499bcc3246a02c8
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-05-17 14:18:03 +0900

    Add cli.go & github.go with method explain

---
+++ cli.go
@@ -0,0 +1,6 @@
+package main
+
+func Run() {
+	// ここで flag 受け取って organization の判定
+	// 判定によって実行する github.go のメソッドを変える
+}
---
+++ github.go
@@ -0,0 +1,9 @@
+package main
+
+func NewGitHubClient() {
+	// go-github で client 生成
+}
+
+func GetEvents() {
+	// flag によってすべて返すか organization のイベントを返すか決める
+}
